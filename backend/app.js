25cee5386fc916e39f18276065e64b2d77540914
Railan <railandejesusdossantos@gmail.com>
2025-11-25 21:26:12 -0300

    configs de rotas adicionada

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+const PORT = 3000
+import router from "./src/routes/auth.js";
+const app = express();
+
+
+app.use("/auth", router)
+
+app.use(express.json());
+
+
+app.listen(PORT, ()=>{
+    console.log("Servidor rodando na porta: " + PORT)
+})
